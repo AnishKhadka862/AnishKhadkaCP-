f7a2709382265edd90837991be2a19ef3e618d53
Anish Khadka <akhadka2@sfsu.edu>
2023-04-07 16:09:48 -0700

    Update benchmark.cpp

--- benchmark.cpp
+++ benchmark.cpp
@@ -6,6 +6,14 @@
 // usage: no command line arguments
 // set problem sizes, block sizes in the code below
 
+//
+// (C) 2022, E. Wes Bethel
+// benchmark-* harness for running different versions of the sum study
+//    over different problem sizes
+//
+// usage: no command line arguments
+// set problem sizes, block sizes in the code below
+
 #include <algorithm>
 #include <chrono>
 #include <iomanip>
@@ -39,16 +47,18 @@ int main(int argc, char** argv)
       // invoke user code to set up the problem
       setup(n, &A[0]);
 
-      // insert your timer code here
+      auto start = std::chrono::steady_clock::now(); // start timer
 
       // invoke method to perform the sum
       t = sum(n, &A[0]);
 
-      // insert your end timer code here, and print out elapsed time for this problem size
+      auto end = std::chrono::steady_clock::now(); // end timer
+
+      // calculate elapsed time and print
+      auto elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+      printf("Elapsed time: %lld ms\n", elapsed_time);
 
       printf(" Sum result = %lld \n",t);
-
    } // end loop over problem sizes
 }
 
-// EOF
