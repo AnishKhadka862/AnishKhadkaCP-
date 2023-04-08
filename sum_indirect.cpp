0979b5727426222ac31bc26b1762297f5272bd2d
Anish Khadka <akhadka2@sfsu.edu>
2023-04-08 16:13:08 -0700

    Update sum_indirect.cpp

--- sum_indirect.cpp
+++ sum_indirect.cpp
@@ -7,29 +7,30 @@
 #include <vector>
 #include <string.h>
 
-
-
 void 
 setup(int64_t N, uint64_t A[])
 {
-   printf(" inside sum_indirect problem_setup, N=%lld \n", N);
+   printf(" inside sum_vector problem_setup, N=%lld \n", N);
+
+   // Populate the vector with random values 
    for (int i = 0; i < N; i++) {
-        A[i] = i;
+        A[i] = lrand48() % N;
    }
 }
 
 int64_t
 sum(int64_t N, uint64_t A[])
 {
-   printf(" inside sum_indirect perform_sum, N=%lld \n", N);
+   printf(" inside sum_vector perform_sum, N=%lld \n", N);
 
    int64_t sum = 0;
+   int indx = A[0];
 
    for(int64_t i=0; i<N; i++)
    {
-      sum += A[i];
+      sum += A[indx];
+      indx = A[indx];
    }
 
    return sum;
-} 
-
+}
