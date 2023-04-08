dc8de5d0d682231cdd629417150812d5454e0907
Anish Khadka <akhadka2@sfsu.edu>
2023-04-08 12:23:49 -0700

    Update sum_indirect.cpp

--- sum_indirect.cpp
+++ sum_indirect.cpp
@@ -12,6 +12,9 @@ void
 setup(int64_t N, uint64_t A[])
 {
    printf(" inside sum_indirect problem_setup, N=%lld \n", N);
+   for (int i = 0; i < N; i++) {
+        A[i] = i;
+   }
 }
 
 int64_t
@@ -23,7 +26,7 @@ sum(int64_t N, uint64_t A[])
 
    for(int64_t i=0; i<N; i++)
    {
-      sum += A[A[i]];
+      sum += A[i];
    }
 
    return sum;
